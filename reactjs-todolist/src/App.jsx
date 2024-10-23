ca4eabe7b1b7b4b33275bf4010f4ceb2c226ff8c
DanielMaherDev <dmdanielmaher@gmail.com>
2024-10-23 00:11:11 +0000

    add function working

--- reactjs-todolist/src/App.jsx
+++ reactjs-todolist/src/App.jsx
@@ -4,14 +4,22 @@ import TodoList from "./components/TodoList"
 
 function App() {
 
-  const [todos, setTodos] = useState([   'Go to the gym',
+  const [todos, setTodos] = useState([  
+    'Go to the gym',
     'Eat more fruits and veg',
-    'Pick up the kids from school'])
+    'Pick up the kids from school'
+  ])
+
+    function handleAddTodos(newTodo) {
+      const newTodoList = [...todos, newTodo]
+      setTodos(newTodoList)
+    }
+
   return ( 
-    <main>
-     <TodoInput />
+    <>
+     <TodoInput handleAddTodos={handleAddTodos}/>
      <TodoList todos={todos} /> 
-    </main>
+    </>
   )
 }
 
